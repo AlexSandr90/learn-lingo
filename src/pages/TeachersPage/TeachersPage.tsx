243352c08dcaf453b17d43fb68e115971873f347
Oleksandr <gayrsuk@gmail.com>
2024-11-21 21:42:34 +0200

    ramoved bgc for teacher list component, added Load more btn

--- src/pages/TeachersPage/TeachersPage.tsx
+++ src/pages/TeachersPage/TeachersPage.tsx
@@ -1,12 +1,14 @@
-import css from './TeachersPage.module.css';
+import css from './TeachersPage.module.scss';
 import Filters from '../../components/Filters/Filters';
 import Teachers from '../../components/Teachers/Teachers';
+import Button from '../../components/Button/Button';
 
 const TeachersPage = () => {
   return (
     <section className={css.teachers_block}>
       <Filters />
       <Teachers />
+      <Button className={css.button}>Load more</Button>
     </section>
   );
 };
