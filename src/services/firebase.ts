50e6d59ab1f04365259d9c12b0cdaadf2501ad8a
Oleksandr <gayrsuk@gmail.com>
2024-12-03 23:40:58 +0200

    deleted commented code

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -24,18 +24,6 @@ const firebaseConfig = {
   databaseURL: VITE_DATABASE_URL,
 };
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyBo8JLh7yUIE1hJg-v4qO2iDYEvYws0iWU',
-//   authDomain: 'learn-lingo-38318.firebaseapp.com',
-//   projectId: 'learn-lingo-38318',
-//   storageBucket: 'learn-lingo-38318.firebasestorage.app',
-//   messagingSenderId: '168383962098',
-//   appId: '1:168383962098:web:cf7a8a73db4e30f02ce96b',
-//   measurementId: 'G-2H9SHBG1TP',
-//   databaseURL:
-//     'https://learn-lingo-38318-default-rtdb.europe-west1.firebasedatabase.app/',
-// };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const database = getDatabase(app);
