f745d73fb609b17401fd2909fec7fa08f0773c7c
Oleksandr <gayrsuk@gmail.com>
2024-12-16 17:16:30 +0200

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,17 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
-const {
-  VITE_APP_ID,
-  VITE_API_KEY,
-  VITE_PROJECT_ID,
-  VITE_AUTH_DOMAIN,
-  VITE_DATABASE_URL,
-  VITE_STORAGE_BUCKET,
-  VITE_MEASUREMENT_ID,
-  VITE_MESSAGING_SENDER_ID,
-} = import.meta.env;
-
 console.log('import.meta.env: ', import.meta.env);
 
 const firebaseConfig = {
