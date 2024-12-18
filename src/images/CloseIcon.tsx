fce358de6e84e10fde96161eb99faa27e50e5745
Oleksandr <gayrsuk@gmail.com>
2024-12-19 01:54:00 +0200

    fixed small bugs

--- src/images/CloseIcon.tsx
+++ src/images/CloseIcon.tsx
@@ -18,16 +18,16 @@ const CloseIcon: React.FC<SvgIconPropTypes> = ({
       <path
         d="M24 8L8 24"
         stroke={color1}
-        stroke-width="2.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M8 8L24 24"
         stroke={color1}
-        stroke-width="2.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
