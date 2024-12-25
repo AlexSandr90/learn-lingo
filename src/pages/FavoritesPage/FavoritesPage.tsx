b8af56b90a9848744c7ed44ec210097748ed034a
Oleksandr <gayrsuk@gmail.com>
2024-12-26 01:43:33 +0200

    removed vite styles, added cover text to Favorite page

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import css from './FavoritesPage.module.scss';
 import { selectFavoritesTeachers } from '../../redux/favorites/selectors';
 import TeacherCard from '../../components/TeacherCard/TeacherCard';
@@ -8,15 +9,25 @@ const FavoritesPage = () => {
 
   return (
     <section className={css.favorites_block}>
+      <h2 className={css.header}>Favorites</h2>
       <ul className={css.teachers_list}>
-        {favoriteTeachers &&
-          favoriteTeachers.length > 0 &&
+        {favoriteTeachers && favoriteTeachers.length === 0 ? (
+          <p className={css.empty_favorites}>
+            Oops! It looks like you haven&apos;t added any teachers to your
+            favorites yet, so we can&apos;t display what isn&apos;t there. Add
+            some favorites and come{' '}
+            <Link to="/teachers" className={css.link}>
+              back
+            </Link>
+          </p>
+        ) : (
           favoriteTeachers.map((teacher) => (
             <TeacherCard
               teacher={teacher}
               key={`${teacher.name}_${teacher.surname}`}
             />
-          ))}
+          ))
+        )}
       </ul>
     </section>
   );
